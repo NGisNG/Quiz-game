7546e526e292c62bb437c6dc99b70095f1b78570
Parwiz Hussaini <p.syas90@yahoo.com>
2024-08-28 16:51:10 +0000

    Add more question option in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,116 @@ const questions = [
             { text: "Africa", correct: false},
         ]
     },
+
+    {
+        question: "which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arktic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
+
+    {
+        question: "which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arktic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
+
+    {
+        question: "which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arktic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
+
+    {
+        question: "which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arktic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
+
+    {
+        question: "which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arktic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
+
+    {
+        question: "which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arktic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
+
+    {
+        question: "which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arktic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
+
+    {
+        question: "which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arktic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
+
+    {
+        question: "which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arktic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
+
+    {
+        question: "which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arktic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
+
+    {
+        question: "which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arktic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
 ];
 
 const questionElement = document.getElementById("question");
@@ -47,14 +157,14 @@ const nextButton = document.getElementById("next-btn");
 let currentQuestionIndex = 0;
 let score = 0;
 
-function startQuiz(){
+function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
-    nextButton.innerHTML = "next";
+    nextButton.innerHTML = "Next Question";
     showQuestion();
 }
 
-function showQuestion(){
+function showQuestion() {
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
@@ -65,7 +175,7 @@ function showQuestion(){
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
-        if(answer.correct){
+        if(answer.correct) {
             button.dataset.correct = answer.correct;
         }
         button.addEventListener("click", selectAnswer);
@@ -80,14 +190,46 @@ function resetState() {
     }
 }
 
-function startQuiz(){
+function selectAnswer(e) {
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+        score++;
     } else{
         selectedBtn.classList.add("incorrect");
     }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    })
+    nextButton.style.display = "block";
 }
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else{
+        startQuiz();
+    }
+});
 
 startQuiz();
